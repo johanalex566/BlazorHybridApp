655ef617f93db7a4fab14f99e6a53a9b50fc145d
Johan Sanchez <johan.sanchez@ghtcorp.com>
2024-12-01 16:09:44 -0500

    Add project files.

---
+++ CoffeeBrowser.Maui/Data/CoffeeService.cs
@@ -0,0 +1,18 @@
+﻿using CoffeeBrowser.Library.Data;
+using System.Net.Http.Json;
+
+namespace CoffeeBrowser.Maui.Data;
+
+public class CoffeeService : ICoffeeService
+{
+
+    private readonly HttpClient _httpClient = new();
+
+    public async Task<IEnumerable<Coffee>?> LoadCoffesAsync()
+    {
+        var coffees = await _httpClient.GetFromJsonAsync<IEnumerable<Coffee?>>("https://fake-coffee-api.vercel.app/api");
+
+        return coffees;
+    }
+
+}
