37ab811340d6b626eac4adbfa3e3efe86bfc722e
Johan Sanchez <johan.sanchez@ghtcorp.com>
2024-12-20 17:24:42 -0500

    Set up dependency injection wpf app

--- CoffeeBrowser.Wpf/App.xaml.cs
+++ CoffeeBrowser.Wpf/App.xaml.cs
@@ -1,14 +1,20 @@
-﻿using System.Configuration;
-using System.Data;
+﻿using Microsoft.Extensions.DependencyInjection;
 using System.Windows;
 
 namespace CoffeeBrowser.Wpf
 {
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
     public partial class App : Application
     {
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+            var services = new ServiceCollection();
+            services.AddWpfBlazorWebView();
+#if DEBUG
+            services.AddBlazorWebViewDeveloperTools();
+#endif
+            var serviceProvider = services.BuildServiceProvider();
+            this.Resources.Add("ServiceProvider", serviceProvider);
+        }
     }
 
 }
